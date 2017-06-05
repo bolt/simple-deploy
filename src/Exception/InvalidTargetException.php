3385fe47fec6441e64504f95d40ab236c2f8f4c2
Gawain Lynch <gawain.lynch@gmail.com>
2017-06-05 18:15:01 +0200

    Exception thrown when an invalid target configuration is requested

---
+++ src/Exception/InvalidTargetException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\SimpleDeploy\Exception;
+
+use RuntimeException;
+
+/**
+ * Exception thrown when an invalid target configuration is requested.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class InvalidTargetException extends RuntimeException
+{
+}
